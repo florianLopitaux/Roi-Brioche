dc1a505e46d73c7d76b30cec41a1363f7eb941b7
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-17 21:47:52 +0100

    Ajout de la barre de recherche et de la gestion de si l'utilisateur s'est déjà connecté

--- Vues/standard/header.php
+++ Vues/standard/header.php
@@ -1,8 +1,13 @@
 <?php
+session_start();
 echo '<header>
     <img src="assets/img/rolling-pin.svg" alt="Rolling pin 3D image to decoration"/>
 
-    <div id="temp-div"></div>
+    <form id="search-bar" method="POST">
+            <img src="../assets/img/search-sharp.svg" alt="loop image in the search bar for design"/>
+            <input type="text" name="search" placeholder="Que cherchez-vous ?"/>
+    </form>';
 
-    <a id="login-btn" class="basic-btn" href="/inscription">Connexion</a>
-</header>';
+// TODO Trouver comment faire pour faire un truc genre index.php?url=utilisateur/email
+if(isset($_SESSION['user'])) echo '<a id="login-btn" class="basic-btn" href="/utilisateur/' . $_SESSION['user'] . '">Profil</a></header>';
+else echo '<a id="login-btn" class="basic-btn" href="/connexion">Connexion</a></header>';
