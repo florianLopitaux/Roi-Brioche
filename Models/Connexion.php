265ac2aa5675cdfd1d231cf29d04ae1c0330608e
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-17 21:50:16 +0100

    Création de la vue avec la gestion de l'affichage

---
+++ Models/Connexion.php
@@ -0,0 +1,33 @@
+<?php
+
+final class Inscription extends Model {
+    private $_S_mail;
+    private $_S_pseudo;
+    private $_S_mdp;
+    private $_D_dateDeDerniereConnexion;
+
+    function __construct($S_mail, $S_pseudo, $S_mdp)
+    {
+        parent::__construct();
+
+        $this->_S_mail = $S_mail;
+        $this->_S_pseudo = $S_pseudo;
+        $this->_S_mdp = $S_mdp;
+
+        $date = date("Y-m-d");
+        $this->_D_dateDeDerniereConnexion = $date;
+    }
+
+    public function insertUser() : string
+    {
+        try {
+            $O_query = $this->getOConnexion()->prepare("INSERT INTO `Utilisateur` (`mail`, `pseudo`, `mdp`, `dateDeCreation`, `dateDeDerniereConnexion`) VALUES (?, ?, ?, ?, ?)");
+            $this->getOConnexion()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->getOConnexion()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+            $O_query->execute(array($this->_S_mail, $this->_S_pseudo, $this->_S_mdp, $this->_D_dateDeCreation, $this->_D_dateDeDerniereConnexion));
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        } return '';
+    }
+}
