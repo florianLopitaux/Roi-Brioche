92b60e54b9308ebe84cc4045a48a53d33b7ff3a9
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 00:51:29 +0100

    Simplification du return

--- Models/RecetteHasard.php
+++ Models/RecetteHasard.php
@@ -11,7 +11,6 @@ class RecetteHasard extends Model
     {
         $O_query = $this->getOConnexion()->query("SELECT `id_Recette`, `nom_Recette`, `difficulte`, `photographie` FROM `Recette` ORDER BY RAND() LIMIT 1");
         $O_query->execute();
-        $A_recette = $O_query->fetch(PDO::FETCH_ASSOC);
-        return $A_recette;
+        return $O_query->fetch(PDO::FETCH_ASSOC);
     }
 }
