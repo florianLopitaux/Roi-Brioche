14c21147c878e15166efdbe0a4f5f849b90ac5fe
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-17 21:49:06 +0100

    Création du controleur avec la gestion de la redirection

---
+++ Controleurs/ControleurConnexion.php
@@ -0,0 +1,13 @@
+<?php
+class ControleurConnexion
+{
+    public function defautAction()
+    {
+        Vue::montrer('connexion/voir', array('' =>  ''));
+    }
+
+    public function connexionAction()
+    {
+
+    }
+}
