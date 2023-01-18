0309adee4b84d1a085728f3c4426a2e98106b6ec
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 19:22:34 +0100

    Ajout de "/"

--- Vues/connexion/voir.php
+++ Vues/connexion/voir.php
@@ -1,13 +1,13 @@
 <?php
-echo '<img id="logo" class="decorate-img" src="assets/img/logo.png" alt="Logo of the website : Roi Brioche"/>
-<img id="cookies" class="decorate-img" src="assets/img/cookies.png" alt="Decoration image representing three cookies"/>
-<img id="donut" class="decorate-img" src="assets/img/donut.png" alt="Decoration image representing a donut"/>
+echo '<img id="logo" class="decorate-img" src="/assets/img/logo.png" alt="Logo of the website : Roi Brioche"/>
+<img id="cookies" class="decorate-img" src="/assets/img/cookies.png" alt="Decoration image representing three cookies"/>
+<img id="donut" class="decorate-img" src="/assets/img/donut.png" alt="Decoration image representing a donut"/>
 
 <main>
     <h1>Se connecter</h1>
 
         <form method="POST">
-            <a id="back-arrow" href="/"><img src="../assets/img/arrow-back-outline.svg" alt="Arrow to return to the home of the website"/></a>
+            <a id="back-arrow" href="/"><img src="/assets/img/arrow-back-outline.svg" alt="Arrow to return to the home of the website"/></a>
 
             <input class="green-input" type="text" name="pseudo" placeholder="Pseudonyme"/>
             <input class="blue-input" type="password" name="password" placeholder="Mot de passe"/>
