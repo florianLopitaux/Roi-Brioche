184f40c5d981c1338a55d3d86eb4b74e8260fd0c
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 19:17:15 +0100

    Passage au Model Recette

--- Controleurs/ControleurDefaut.php
+++ Controleurs/ControleurDefaut.php
@@ -4,8 +4,8 @@ class ControleurDefaut
 {
     public function defautAction()
     {
-        $O_recetteHasard = new RecetteHasard();
-        Vue::montrer('index/voir', array('allRecettesHasard' =>  array($O_recetteHasard->getRecetteHasard(), $O_recetteHasard->getRecetteHasard(), $O_recetteHasard->getRecetteHasard())));
+        $O_recetteHasard = new Recette();
+        Vue::montrer('index/voir', array('allRecettesHasard' => $O_recetteHasard->get3RecettesResume()));
     }
 
 }
