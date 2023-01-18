d3a26a953285a280f5bd26256c1b556ece658bd6
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 23:16:27 +0100

    Ajout de l'action pour le formulaire

--- Vues/standard/header.php
+++ Vues/standard/header.php
@@ -3,7 +3,7 @@ session_start();
 echo '<header>
     <img id="rolling-pin" src="../assets/img/rolling-pin.svg" alt="Rolling pin 3D image to decoration"/>
 
-        <form id="search-bar" method="POST">
+        <form action="/recette/recherche" id="search-bar" method="POST">
             <img src="../assets/img/search-sharp.svg" alt="loop image in the search bar for design"/>
             <input type="text" name="search" placeholder="Que cherchez-vous ?"/>
         </form>';
