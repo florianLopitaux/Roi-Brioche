c77f64bc3bb6c7226cf4d27262d71e1cd5b0defa
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 21:15:22 +0100

    Ajout de la gestion de la connexion

--- Controleurs/ControleurConnexion.php
+++ Controleurs/ControleurConnexion.php
@@ -6,8 +6,20 @@ class ControleurConnexion
         Vue::montrer('connexion/voir', array('' =>  ''));
     }
 
-    public function connexionAction()
+    public function connexionAction($A_getParam, $A_postParam)
     {
+        $S_email = !empty($A_postParam['email']) ? $A_postParam['email'] : null;
+        $S_password = !empty($A_postParam['password']) ? $A_postParam['password'] : null;
 
+        $O_user = new Utilisateur();
+        if ($O_user->checkForUser($S_email, $S_password) == 'Aucune erreur') {
+            $_SESSION['user'] = $S_email;
+            header('Location: /');
+            exit();
+        }
+        else {
+            header('Location: /inscription');
+            exit();
+        }
     }
 }
