225ac1b061fc5437546c3e5093d074ee61100dd2
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 02:20:13 +0100

    Début de la vu sans le formulaire et sans la mise en forme

--- Vues/recette/voir.php
+++ Vues/recette/voir.php
@@ -1,1 +1,10 @@
 <?php
+Vue::montrer('standard/header');
+
+echo '<link rel="stylesheet" href="css/index_style.css"/>';
+
+foreach ($A_vue['allRecettesResume'] as $O_recetteResume) {
+    Vue::montrer('standard/recetteResume', array('recette' =>  $O_recetteResume));
+}
+
+Vue::montrer('standard/pied');
