17af84e8b27b2473b601589d5f41cd8b03db399d
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 19:15:45 +0100

    Suppression de la partie objet

--- Models/Appreciation.php
+++ Models/Appreciation.php
@@ -1,58 +1,23 @@
 <?php
 
 final class Appreciation extends Model {
-    private $_I_Id_Recette;
-    private $_S_mail;
 
     function __construct(){
         parent::__construct();
     }
 
-    public static function withIdRecette(int $I_Id_Recette) : Appreciation
-    {
-        $O_instance = new self();
-        $O_instance->_I_Id_Recette = $I_Id_Recette;
-        return $O_instance;
-    }
-
-    public static function withMail(string $S_mail) {
-        $O_instance = new self();
-        $O_instance->_S_mail = $S_mail;
-        return $O_instance;
-    }
-
-    public function getAppreciationByIdRecette() : array
+    public function getAppreciationByIdRecette($I_id_Recette) : array
     {
         $O_query = $this->getOConnexion()->query('SELECT `id_Recette`, `mail`, `note`, `commentaire`, `dateDeCreation` FROM `Appreciation` WHERE `id_Recette` = ?');
-        $O_query->execute(array($this->_I_Id_Recette));
+        $O_query->execute(array($I_id_Recette));
 
-        $A_Appreciations = array();
-        while ($A_results = $O_query->fetch(PDO::FETCH_ASSOC)) {
-            $A_Appreciations[] = array(
-                'id_Recette' => $A_results['id_Recette'],
-                'mail' => $A_results['mail'],
-                'note' => $A_results['note'],
-                'commentaire' => $A_results['commentaire'],
-                'dateDeCreation' => $A_results['dateDeCreation']
-            );
-        }
-        return $A_Appreciations;
+        return $O_query->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function getAppreciationByMail() : array {
+    public function getAppreciationByMail($S_mail) : array {
         $O_query = $this->getOConnexion()->query('SELECT `id_Recette`, `mail`, `note`, `commentaire`, `dateDeCreation` FROM `Appreciation` WHERE `mail` = ?');
-        $O_query->execute(array($this->_S_mail));
+        $O_query->execute(array($S_mail));
 
-        $A_Appreciations = array();
-        while ($A_results = $O_query->fetch(PDO::FETCH_ASSOC)) {
-            $A_Appreciations[] = array(
-                'id_Recette' => $A_results['id_Recette'],
-                'mail' => $A_results['mail'],
-                'note' => $A_results['note'],
-                'commentaire' => $A_results['commentaire'],
-                'dateDeCreation' => $A_results['dateDeCreation']
-            );
-        }
-        return $A_Appreciations;
+        return $O_query->fetchAll(PDO::FETCH_ASSOC);
     }
 }
