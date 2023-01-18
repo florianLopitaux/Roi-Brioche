1011d6ab69205e2be85570a1893ed4aab010a226
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 02:22:04 +0100

    Ajout de l'action de base et de l'action quand on clique sur un résume de recette

--- Controleurs/ControleurRecette.php
+++ Controleurs/ControleurRecette.php
@@ -2,5 +2,16 @@
 
 class ControleurRecette
 {
+    public function defautAction()
+    {
+        $O_recette = new Recette();
+        Vue::montrer('recette/voir', array('allRecettesResume' =>  $O_recette->getAllRecettesResume()));
+    }
 
+    // TODO Après avoir demander pour le $A_getParams, voir si c'est la bonne façon de faire avec $I_id
+    public function voirAction(int $I_Id)
+    {
+        $O_recette = new Recette();
+        Vue::montrer('recette/voir', array('recette' =>  $O_recette->getRecetteById($I_Id)));
+    }
 }
