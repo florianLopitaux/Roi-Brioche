361815c81af7b8f932ca0bc540c271bd1f139f8e
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 23:15:13 +0100

    Ajout de la gestion des recherches

--- Controleurs/ControleurRecette.php
+++ Controleurs/ControleurRecette.php
@@ -5,7 +5,7 @@ class ControleurRecette
     public function defautAction()
     {
         $O_recette = new Recette();
-        Vue::montrer('recette/voir', array('allRecettesResume' =>  $O_recette->getAllRecettesResume()));
+        Vue::montrer('recette/voir', array('allRecettesResume' =>  $O_recette->getAllRecettesResume(), 'titre' => 'Toutes les recettes'));
     }
 
     public function voirAction($A_getParam, $A_postParam)
@@ -17,5 +17,16 @@ class ControleurRecette
 
         $O_recette = new Recette();
         Vue::montrer('recette/recette', array('recette' =>  $O_recette->getRecetteById($A_getParam[0])));
+    }
+
+    public function rechercheAction($A_getParam, $A_postParam)
+    {
+        if ($A_postParam['search'] == '') {
+            header('Location: /');
+            exit();
+        }
+
+        $O_recette = new Recette();
+        Vue::montrer('recette/voir', array('allRecettesResume' =>  $O_recette->getAllRecettesResumeByRecherche($A_postParam['search']), 'titre' => 'Résultat pour : ' . $A_postParam['search']));
     }
 }
