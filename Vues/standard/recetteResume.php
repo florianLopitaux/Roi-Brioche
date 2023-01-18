07b9adc6affe79b5a7844be54530ecf5cfce718a
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 19:21:52 +0100

    Ajout de la redirection vers la vue de la recette complète

--- Vues/standard/recetteResume.php
+++ Vues/standard/recetteResume.php
@@ -1,7 +1,6 @@
 <?php
-echo '<div class="recipe">
+echo '<a class="recipe" href="/recette/voir/' . $A_vue['recette']['id_Recette'] . '">
     <img src="data:image/jpeg;base64,' . base64_encode($A_vue['recette']['photographie']) . '" alt="image representing the food described by the recipe"/>
     <h3>' . $A_vue['recette']['nom_Recette'] . '</h3>
     <p>Difficulté : ' . $A_vue['recette']['difficulte'] . '</p>
-</div>';
-// TODO Ajouter un input hidden pour l'id de la recette
+</a>';
