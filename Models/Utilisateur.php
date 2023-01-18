22539f1282a7e2f80f962949ef0db11e44b24928
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 21:19:48 +0100

    Suppression de la partie objet et ajout de la fonction pour la connexion

--- Models/Utilisateur.php
+++ Models/Utilisateur.php
@@ -2,37 +2,49 @@
 
 class Utilisateur extends Model
 {
-    private $_S_mail;
-    private $_S_pseudo;
-    private $_S_mdp;
-    private $_D_dateDeCreation;
-    private $_D_dateDeDerniereConnexion;
-    private $_B_photographie;
 
-    function __construct($S_mail, $S_pseudo, $S_mdp)
+    function __construct()
     {
         parent::__construct();
-
-        $this->_S_mail = $S_mail;
-        $this->_S_pseudo = $S_pseudo;
-        $this->_S_mdp = $S_mdp;
 //        $this->_B_photographie = $B_photographie;
     }
 
-    public function insertUser() : string
+    public function insertUser($S_mail, $S_pseudo, $S_mdp) : string
     {
-        $date = date("Y-m-d");
-        $this->_D_dateDeCreation = $date;
-        $this->_D_dateDeDerniereConnexion = $date;
+        $D_date = date("Y-m-d");
 
         try {
             $O_query = $this->getOConnexion()->prepare("INSERT INTO `Utilisateur` (`mail`, `pseudo`, `mdp`, `dateDeCreation`, `dateDeDerniereConnexion`) VALUES (?, ?, ?, ?, ?)");
             $this->getOConnexion()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->getOConnexion()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-            $O_query->execute(array($this->_S_mail, $this->_S_pseudo, $this->_S_mdp, $this->_D_dateDeCreation, $this->_D_dateDeDerniereConnexion));
+            $O_query->execute(array($S_mail, $S_pseudo, $S_mdp, $D_date, $D_date));
         } catch (PDOException $e) {
             return $e->getMessage();
         } return 'Aucune erreur';
+    }
+
+    public function checkForUser($S_mail, $S_password) : string
+    {
+        $D_date = date("Y-m-d");
+
+        try {
+            $O_query = $this->getOConnexion()->prepare("SELECT `mdp` FROM `Utilisateur` WHERE `mail` = ?");
+            $this->getOConnexion()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->getOConnexion()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+            $O_query->execute(array($S_mail));
+            $A_result = $O_query->fetch(PDO::FETCH_ASSOC);
+
+            if (password_verify($S_password, $A_result['mdp'])) {
+                $O_query = $this->getOConnexion()->prepare("UPDATE `Utilisateur` SET `dateDeDerniereConnexion` = ? WHERE `mail` = ?");
+                $O_query->execute(array($D_date, $S_mail));
+                return 'Aucune erreur';
+            } else {
+                return $A_result['mdp'];
+            }
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
     }
 }
