a71ca3628832192e760b9fe0ad6db0d35e22815d
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 02:19:30 +0100

    changement pour que ça soit plus universel

--- Vues/standard/recetteResume.php
+++ Vues/standard/recetteResume.php
@@ -1,6 +1,6 @@
 <?php
 echo '<div class="recipe">
-    <img src="data:image/jpeg;base64,' . base64_encode($A_vue['recetteHasard']['photographie']) . '" alt="image representing the food described by the recipe"/>
-    <h3>' . $A_vue['recetteHasard']['nom_Recette'] . '</h3>
-    <p>Difficulté : ' . $A_vue['recetteHasard']['difficulte'] . '</p>
+    <img src="data:image/jpeg;base64,' . base64_encode($A_vue['recette']['photographie']) . '" alt="image representing the food described by the recipe"/>
+    <h3>' . $A_vue['recette']['nom_Recette'] . '</h3>
+    <p>Difficulté : ' . $A_vue['recette']['difficulte'] . '</p>
 </div>';
