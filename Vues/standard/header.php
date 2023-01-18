406c3e6eda55f06f516822b9009d6a5b08cadf0e
Jules <jules.combet@etu.univ-amu.fr>
2023-01-18 12:15:28 +0100

    rajout d'id

--- Vues/standard/header.php
+++ Vues/standard/header.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 echo '<header>
-    <img src="assets/img/rolling-pin.svg" alt="Rolling pin 3D image to decoration"/>
+    <img id="rolling-pin" src="../assets/img/rolling-pin.svg" alt="Rolling pin 3D image to decoration"/>
 
-    <form id="search-bar" method="POST">
-            <img src="assets/img/search-sharp.svg" alt="loop image in the search bar for design"/>
+        <form id="search-bar" method="POST">
+            <img src="../assets/img/search-sharp.svg" alt="loop image in the search bar for design"/>
             <input type="text" name="search" placeholder="Que cherchez-vous ?"/>
-    </form>';
+        </form>';
 
-// TODO Trouver comment faire pour faire un truc genre index.php?url=utilisateur/email
-if(isset($_SESSION['user'])) echo '<a id="login-btn" class="basic-btn" href="/utilisateur/' . $_SESSION['user'] . '">Profil</a></header>';
+if(isset($_SESSION['user'])) echo '<a id="login-btn" class="basic-btn" href="/utilisateur/voir/' . $_SESSION['user'] . '">Profil</a></header>';
 else echo '<a id="login-btn" class="basic-btn" href="/connexion">Connexion</a></header>';
