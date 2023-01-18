a060631277b0846b5c4cffac1a4fa7082a4a00b3
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-18 19:19:46 +0100

    Ajout de "/"

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -8,7 +8,7 @@
     <title>Roi Brioche</title>
 
     <!-- STYLE FILES -->
-    <link rel="stylesheet" href="css/global_style.css"/>
+    <link rel="stylesheet" href="/css/global_style.css"/>
 
     <?php
     $A_url_param = explode('/', $A_vue['url']);
@@ -17,7 +17,7 @@
     if (sizeof($A_url_param) >= 2) {
         $I_index_page = 1;
     } elseif ($A_url_param[0] == 'index' || $A_url_param[0] == 'recette') {
-        echo '<link rel="stylesheet" href="css/recipe_preview_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/recipe_preview_style.css"/>';
     }
 
     echo '<link rel="stylesheet" href="css/' . $A_url_param[$I_index_page] . '_style.css"/>';
