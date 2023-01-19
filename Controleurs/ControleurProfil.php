eb1028e5c78e2e140b4b1b60513945f5785abc71
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-19 22:37:08 +0100

    Ajout du controleur pour l'affichage du profil avec une verification de si l'utilisateur est log

---
+++ Controleurs/ControleurProfil.php
@@ -0,0 +1,24 @@
+<?php
+
+class ControleurProfil
+{
+    public function defautAction()
+    {
+        if (isset($_SESSION['user'])) {
+            $O_utilisateur = new Utilisateur();
+            $A_utilisateur = $O_utilisateur->getUser($_SESSION['user']);
+
+            if (sizeof($A_utilisateur['appreciations']) == 0) {
+                $titre = 'Aucun commentaire';
+            } else {
+                $titre = 'Commentaires ( ' . sizeof($A_utilisateur['appreciations']) . ' )';
+            }
+
+            Vue::montrer('profil/voir', array('profil' => $O_utilisateur->getUser($_SESSION['user']), 'titre' => $titre));
+        }
+        else {
+            header('Location: /connexion');
+            exit();
+        }
+    }
+}
