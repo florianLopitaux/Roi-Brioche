380493d8a0eeb7861e47a0504045ca57d2794392
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-20 17:42:54 +0100

    Ajout du nombre de recette dans la recherche et dans "toutes les recettes"

--- Controleurs/ControleurRecette.php
+++ Controleurs/ControleurRecette.php
@@ -5,7 +5,8 @@ class ControleurRecette
     public function defautAction()
     {
         $O_recette = new Recette();
-        Vue::montrer('recette/voir', array('allRecettesResume' =>  $O_recette->getAllRecettesResume(), 'titre' => 'Toutes les recettes'));
+        $A_recette = $O_recette->getAllRecettesResume();
+        Vue::montrer('recette/voir', array('allRecettesResume' =>  $A_recette, 'titre' => 'Toutes les recettes (' . sizeof($A_recette) . ')'));
     }
 
     public function voirAction($A_getParam, $A_postParam)
@@ -32,7 +33,7 @@ class ControleurRecette
         if (sizeof($A_recettes) == 0) {
             $titre = 'Aucune recette trouvée';
         } else {
-            $titre = 'Résultats pour : "' . $_GET['search'] . '"';
+            $titre = 'Résultats pour : "' . $_GET['search'] . '" (' . sizeof($A_recettes) . ')';
         }
         Vue::montrer('recette/voir', array('allRecettesResume' =>  $A_recettes, 'titre' => $titre));
     }
