e0d9374b727533f2aa57e4d30e0eafad9f194b01
florianLopitaux <florian.lopitaux@etu.univ-amu.fr>
2023-01-22 16:07:02 +0100

    change link style file system to adapt to the future re-organization

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -1,5 +1,26 @@
+<?php
+function get_style_dependencies() : array {
+    $A_style_files = [];
+
+    $A_url_param = explode('/', $A_vue['url']);
+    $I_index_page = sizeof($A_url_param) < 2 ? 0 : 1;
+
+    if ($A_url_param[$I_index_page] === 'connexion' || $A_url_param[$I_index_page] === 'inscription') {
+        $A_style_files[] = '/css_compiled/pages/login_style.css';
+    } else {
+        $A_style_files[] = '/css_compiled/pages/' . $A_url_param[$I_index_page] . '_style.css';
+    }
+
+    if (in_array($A_url_param[$I_index_page], ['index', 'recette', 'recherche'])) {
+        $A_style_files[] = '/css_compiled/standard_elements/recipe_preview_style.css';
+    }
+    
+    return $A_style_files;
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,27 +29,16 @@
     <title>Roi Brioche</title>
 
     <!-- STYLE FILES -->
-    <link rel="stylesheet" href="/css/global_style.css"/>
+    <link rel="stylesheet" href="/css_compiled/global_style.css">
+    <link rel="stylesheet" href="/css_compiled/standard_elements/header_style.css">
+    <link rel="stylesheet" href="/css_compiled/standard_elements/footer_style.css">
 
-    <?php
-    $A_url_param = explode('/', $A_vue['url']);
-    $I_index_page = 0;
-
-    if (sizeof($A_url_param) >= 2) {
-        $I_index_page = 1;
-        if ($A_url_param[1] == 'recherche') {
-            echo '<link rel="stylesheet" href="/css/recipe_preview_style.css"/>';
-            $I_index_page = 0;
-        }
-    } elseif ($A_url_param[0] == 'index' || $A_url_param[0] == 'recette') {
-        echo '<link rel="stylesheet" href="/css/recipe_preview_style.css"/>';
-    }
-
-    echo '<link rel="stylesheet" href="/css/' . $A_url_param[$I_index_page] . '_style.css"/>';
-    ?>
-
+    <?php foreach(get_style_dependencies() as $S_file_style): ?>
+        <link rel="stylesheet" href="<?= $S_file_style ?>">
+    <?php endforeach ?>
 </head>
-    <body>
-        <?php echo $A_vue['body'] ?>
-    </body>
+
+<body>
+    <?php echo $A_vue['body'] ?>
+</body>
 </html>
