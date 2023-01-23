bbab3ab1ce7b55a2adcf38e5f9da52a3df3b0c97
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-23 22:50:22 +0100

    Ajout de la recherche de profil et de la visualisation d'un profil si on est admin

--- Controleurs/ControleurProfil.php
+++ Controleurs/ControleurProfil.php
@@ -4,21 +4,50 @@ class ControleurProfil
 {
     public function defautAction()
     {
-        if (isset($_SESSION['user']) || $_SESSION['statut'] == 'administrateur') {
+        if ($_SESSION['statut'] == 'administrateur' && isset($_GET['search']) && $_GET['search'] != '') {
             $O_utilisateur = new Utilisateur();
-            $A_utilisateur = $O_utilisateur->getUser($_SESSION['user']);
+            $A_utilisateurs = $O_utilisateur->getUserByNameOrEmail($_GET['search']);
 
-            if (sizeof($A_utilisateur['appreciations']) == 0) {
-                $titre = 'Aucun commentaire';
+            if (sizeof($A_utilisateurs) == 0) {
+                $titre = 'Aucun profil trouvé';
             } else {
-                $titre = 'Commentaires ( ' . sizeof($A_utilisateur['appreciations']) . ' )';
+                $titre = 'Profils ( ' . sizeof($A_utilisateurs) . ' )';
             }
 
-            Vue::montrer('profil/voir', array('profil' => $O_utilisateur->getUser($_SESSION['user']), 'titre' => $titre));
+            Vue::montrer('profil/voir', array('profils' => $A_utilisateurs, 'titre' => $titre));
+        }
+        elseif (isset($_SESSION['user'])) {
+            $this->showUser($_SESSION['user']);
         }
         else {
             header('Location: /connexion');
             exit();
         }
+    }
+
+    public function voirAction($A_getParam, $A_postParam)
+    {
+        if ($_SESSION['statut'] == 'administrateur' && isset($A_getParam[0]) && $A_getParam[0] != '') {
+            $this->showUser($A_getParam[0]);
+        }
+        else {
+            header('Location: /profil');
+            exit();
+        }
+    }
+
+    public function showUser(string $mail): void
+    {
+        $O_utilisateur = new Utilisateur();
+        $A_utilisateurs = $O_utilisateur->getUser($mail);
+
+        if (sizeof($A_utilisateurs) == 0) {
+            $titre = 'Aucun profil trouvé';
+        }
+        else {
+            $titre = 'Profils ( ' . sizeof($A_utilisateurs) . ' )';
+        }
+
+        Vue::montrer('profil/voir', array('profil' => $A_utilisateurs, 'titre' => $titre));
     }
 }
