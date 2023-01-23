51348c0023c29082da8b7e16d53b741c14b3f95a
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-23 21:32:19 +0100

    Ajout de la gestion des images avec le type et la taille

--- Controleurs/ControleurInscription.php
+++ Controleurs/ControleurInscription.php
@@ -18,14 +18,21 @@ class ControleurInscription
         $S_email = !empty($A_postParam['email']) ? $A_postParam['email'] : null;
         $S_password = !empty($A_postParam['password']) ? $A_postParam['password'] : null;
         $S_check_password = !empty($A_postParam['verif_password']) ? $A_postParam['verif_password'] : null;
+        $S_image = !empty($_FILES['photographie']) ? $_FILES['photographie'] : null;
 
-        header("Content-type: image/png");
-        $S_photographie = !empty($A_postParam['photographie']) ? $A_postParam['photographie'] : null;
+        if ($S_password == $S_check_password && $S_pseudo != null && $S_email != null && $S_password != null && $S_check_password != null && $S_image != null) {
+            $S_imageData = ($S_image['tmp_name']);
+            list($S_width, $S_height, $S_type, $S_attr) = getimageSize($S_image['tmp_name']);
+            $A_allowTypes = array('image/jpg','image/png','image/jpeg');
+            $S_imageType = getimagesize($S_image['tmp_name'])['mime'];
 
+            if ($S_imageType == null || !in_array($S_imageType, $A_allowTypes) || $S_width != 400 || $S_width != $S_height) {
+                header('Location: /inscription');
+                exit();
+            }
 
-        if ($S_password == $S_check_password && $S_pseudo != null && $S_email != null && $S_password != null && $S_check_password != null && image_type_to_extension()) {
             $O_user = new Utilisateur();
-            if ($O_user->insertUser($S_email, $S_pseudo, password_hash($S_password, PASSWORD_BCRYPT)) != 'Aucune erreur') {
+            if ($O_user->insertUser($S_email, $S_pseudo, password_hash($S_password, PASSWORD_BCRYPT), file_get_contents($S_imageData)) != 'Aucune erreur') {
                 header('Location: /inscription');
             } else {
                 $_SESSION['user'] = $S_email;
