84330b3147ef5c1941074dfcdf98a55151c085e8
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-23 23:04:13 +0100

    Changement des noms des variables

--- Vues/profil/profilResume.php
+++ Vues/profil/profilResume.php
@@ -1,6 +1,6 @@
 <?php
-echo '<a class="user" href="/profil/voir/' . $A_vue['recette']['id_Recette'] . '">
-    <img src="data:image/jpeg;base64,' . base64_encode($A_vue['recette']['photographie']) . '" alt="image representing the food described by the recipe"/>
-    <h3>' . $A_vue['recette']['nom_Recette'] . '</h3>
-    <p>Difficulté : ' . $A_vue['recette']['difficulte'] . '</p>
+echo '<a class="user" href="/profil/voir/' . $A_vue['profil']['id_Recette'] . '">
+    <img src="data:image/jpeg;base64,' . base64_encode($A_vue['profil']['photographie']) . '" alt="image representing the food described by the recipe"/>
+    <h1>' . $A_vue['profil']['mail'] . '</h1>
+    <p>Pseudo : ' . $A_vue['profil']['pseudo'] . '</p>
 </a>';
