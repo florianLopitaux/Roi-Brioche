e8a1528c8274b2d1efd0c0567fb2d053e5d3f396
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-23 21:36:51 +0100

    Ajout d'insert du blob

--- Models/Utilisateur.php
+++ Models/Utilisateur.php
@@ -6,19 +6,18 @@ class Utilisateur extends Model
     function __construct()
     {
         parent::__construct();
-//        $this->_B_photographie = $B_photographie;
     }
 
-    public function insertUser(string $S_mail, string $S_pseudo, string $S_mdp) : string
+    public function insertUser(string $S_mail, string $S_pseudo, string $S_mdp, string $B_photographie) : string
     {
         $D_date = date("Y-m-d");
 
         try {
-            $O_query = $this->getOConnexion()->prepare("INSERT INTO `Utilisateur` (`mail`, `pseudo`, `mdp`, `dateDeCreation`, `dateDeDerniereConnexion`) VALUES (?, ?, ?, ?, ?)");
+            $O_query = $this->getOConnexion()->prepare("INSERT INTO `Utilisateur` (`mail`, `pseudo`, `mdp`, `dateDeCreation`, `dateDeDerniereConnexion`, `photographie`) VALUES (?, ?, ?, ?, ?, ?)");
             $this->getOConnexion()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->getOConnexion()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-            $O_query->execute(array($S_mail, $S_pseudo, $S_mdp, $D_date, $D_date));
+            $O_query->execute(array($S_mail, $S_pseudo, $S_mdp, $D_date, $D_date, $B_photographie));
         } catch (PDOException $e) {
             return $e->getMessage();
         } return 'Aucune erreur';
@@ -29,7 +28,7 @@ class Utilisateur extends Model
         $D_date = date("Y-m-d");
 
         try {
-            $O_query = $this->getOConnexion()->prepare("SELECT `mdp` FROM `Utilisateur` WHERE `mail` = ?");
+            $O_query = $this->getOConnexion()->prepare("SELECT `mdp`, `statut` FROM `Utilisateur` WHERE `mail` = ?");
             $this->getOConnexion()->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->getOConnexion()->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
@@ -39,6 +38,7 @@ class Utilisateur extends Model
             if (password_verify($S_password, $A_result['mdp'])) {
                 $O_query = $this->getOConnexion()->prepare("UPDATE `Utilisateur` SET `dateDeDerniereConnexion` = ? WHERE `mail` = ?");
                 $O_query->execute(array($D_date, $S_mail));
+                $_SESSION['statut'] = $A_result['statut'];
                 return 'Aucune erreur';
             } else {
                 return 'Mot de passe incorrect';
