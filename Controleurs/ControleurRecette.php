53e3626b805134d401e62ac6746add8e9a2ea64d
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-23 22:50:46 +0100

    Changement de Changement du /recette/recherche/?search= par /recette/?search=

--- Controleurs/ControleurRecette.php
+++ Controleurs/ControleurRecette.php
@@ -5,31 +5,30 @@ class ControleurRecette
     public function defautAction($A_getParam, $A_postParam)
     {
         $O_recette = new Recette();
-        if (sizeof($A_getParam) == 0 or $A_getParam[0] == '') {
+        if (!isset($_GET['search']) or $_GET['search'] == '') {
             $A_recette = $O_recette->getAllRecettesResume();
             Vue::montrer('recette/voir', array('allRecettesResume' =>  $A_recette, 'titre' => 'Toutes les recettes (' . sizeof($A_recette) . ')'));
         }
         else {
-            Vue::montrer('recette/recette', array('recette' =>  $O_recette->getRecetteById($A_getParam[0])));
-        }
+            $A_recettes = $O_recette->getAllRecettesResumeByRecherche($_GET['search']);
 
+            if (sizeof($A_recettes) == 0) {
+                $titre = 'Aucune recette trouvée';
+            } else {
+                $titre = 'Résultats pour : "' . $_GET['search'] . '" (' . sizeof($A_recettes) . ')';
+            }
+            Vue::montrer('recette/voir', array('allRecettesResume' =>  $A_recettes, 'titre' => $titre));
+        }
     }
 
-    public function rechercheAction($A_getParam, $A_postParam)
+    public function voirAction($A_getParam, $A_postParam)
     {
-        if ($_GET['search'] == '') {
-            header('Location: /');
+        if (sizeof($A_getParam) == 0 or $A_getParam[0] == '') {
+            header('Location: /recette');
             exit();
         }
 
         $O_recette = new Recette();
-        $A_recettes = $O_recette->getAllRecettesResumeByRecherche($_GET['search']);
-
-        if (sizeof($A_recettes) == 0) {
-            $titre = 'Aucune recette trouvée';
-        } else {
-            $titre = 'Résultats pour : "' . $_GET['search'] . '" (' . sizeof($A_recettes) . ')';
-        }
-        Vue::montrer('recette/voir', array('allRecettesResume' =>  $A_recettes, 'titre' => $titre));
+        Vue::montrer('recette/recette', array('recette' =>  $O_recette->getRecetteById($A_getParam[0])));
     }
 }
