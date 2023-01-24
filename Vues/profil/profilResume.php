77b4b0683c53893b72b2444298523e6817fa7888
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-24 18:07:26 +0100

    Début de preview du profil

--- Vues/profil/profilResume.php
+++ Vues/profil/profilResume.php
@@ -1,5 +1,11 @@
 <?php
-echo '<a class="user" href="/profil/voir/' . $A_vue['profil']['id_Recette'] . '">
+if ($_SESSION['statut'] == 'administateur') {
+    $S_link = '/profil/voir/' . $A_vue['profil']['mail'];
+} else {
+    $S_link = '/profil';
+}
+
+echo '<a class="user" href="' . $S_link . '">
     <img src="data:image/jpeg;base64,' . base64_encode($A_vue['profil']['photographie']) . '" alt="image representing the food described by the recipe"/>
     <h1>' . $A_vue['profil']['mail'] . '</h1>
     <p>Pseudo : ' . $A_vue['profil']['pseudo'] . '</p>
