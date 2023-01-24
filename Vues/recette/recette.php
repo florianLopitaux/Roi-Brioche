d7fa6ddb3e308f9218ddbc284250e458e41b6fcd
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-24 16:37:01 +0100

    Avancement de la recette

--- Vues/recette/recette.php
+++ Vues/recette/recette.php
@@ -24,52 +24,58 @@ echo '<span id="commentaries">' . sizeof($A_vue['recette']['appreciations']) . '
             </div>
 
             <div id="bottom-recipe-resume">
-                <span class="preparation-data"><strong>Temps de préparation:</strong> 30min</span>
-                <span class="preparation-data"><strong>Difficulté:</strong> facile</span>
-                <span class="preparation-data"><strong>Prix:</strong> Bon marché</span>
-                <span class="preparation-data"><strong>Type de cuisson:</strong> Au four</span>
-                <span class="preparation-data"><strong>Allergies:</strong> Aucune</span>
+                <span class="preparation-data"><strong>Temps de préparation : </strong> ' . $A_vue['recette']['tempsDePreparation'] . 'min</span>
+                <span class="preparation-data"><strong>Difficulté : </strong> ' . $A_vue['recette']['difficulte'] . '</span>
+                <span class="preparation-data"><strong>Prix : </strong> ' . $A_vue['recette']['cout'] . '</span>
+                <span class="preparation-data"><strong>Type de cuisson : </strong> ' . $A_vue['recette']['typeDeCuisson'] . '</span>
+                <span class="preparation-data"><strong>Allergies : </strong> ';
+
+if ($A_vue['recette']['particularites'] != null) {
+    for ($i = 0; $i < sizeof($A_vue['recette']['particularites']) - 1; $i++) {
+        echo $A_vue['recette']['particularites'][$i]['nom_Particularite'] . ', ';
+    }
+    echo end($A_vue['recette']['particularites'])['nom_Particularite'];
+}
+else {
+    echo 'Aucune';
+}
+
+echo '</span>
             </div>
         </section>
 
         <section class="data-section">
             <h3>Ingrédients</h3>
 
-            <section class="data-list">
-                <span class="recipe-data">415g de <strong>farine</strong></span>
-                <span class="recipe-data">1.5 c.a.c de <strong>levure chimique</strong></span>
-                <span class="recipe-data">2/3 c.a.c de <strong>sel</strong></span>
-                <span class="recipe-data">65g de <strong>sucre en poudre</strong></span>
-                <span class="recipe-data">23cl de <strong>lait</strong></span>
-                <span class="recipe-data">2 <strong>oeufs</strong></span>
-                <span class="recipe-data">50g de <strong>beurre</strong></span>
-                <span class="recipe-data">2/3l <strong>d\'huile</strong></span>
-            </section>
+            <section class="data-list">';
+
+foreach ($A_vue['recette']['ingredients'] as $ingredient) {
+    echo '<span class="recipe-data"><strong>' . $ingredient['nom_Ingredient'] . '</strong></span>';
+}
+
+echo '</section>
         </section>
 
         <section class="data-section">
             <h3>Ustensiles</h3>
 
-            <section class="data-list">
-                <span class="recipe-data">1 <strong>saladier</strong></span>
-                <span class="recipe-data">1 <strong>cuillière</strong></span>
-                <span class="recipe-data">2 <strong>couteau</strong></span>
-                <span class="recipe-data">1 <strong>casserole</strong></span>
-                <span class="recipe-data">1 <strong>pinceau</strong></span>
-                <span class="recipe-data">1 <strong>balance de cuisine</strong></span>
-            </section>
+            <section class="data-list">';
+
+foreach ($A_vue['recette']['ustensiles'] as $ustensile) {
+    echo '<span class="recipe-data"><strong>' . $ustensile['nom_Ustensile'] . '</strong></span>';
+}
+
+echo '</section>
         </section>
 
         <section class="data-section">
             <h3>Préparation</h3>
 
-            <section id="preparation-list" class="data-list">
-                <h4>Etape 1</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto molestiae adipisci repellat consequatur ducimus? Reiciendis officia sint fugiat obcaecati placeat eum quaerat quia voluptatibus delectus tenetur! Molestias impedit asperiores nobis.</p>
+            <section id="preparation-list" class="data-list">';
 
-                <h4>Etape 2</h4>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora dolorem consequuntur explicabo a libero mollitia voluptate provident delectus sequi modi laboriosam quod magnam in dolor, cumque nam velit dicta omnis?</p>
-            </section>
+echo $A_vue['recette']['description'];
+
+echo '</section>
         </section>
 
         <section id="commentaries" class="data-section">
@@ -80,18 +86,5 @@ echo '<span id="commentaries">' . sizeof($A_vue['recette']['appreciations']) . '
             </section>
         </section>
     </main>';
-
-echo '<p>Id :' . $A_vue['recette']['id_Recette'] . '</p>';
-echo '<p>Nom :' . $A_vue['recette']['nom_Recette'] . '</p>';
-echo '<p>moyenne :' . $A_vue['recette']['moyenne'] . '</p>';
-echo '<p>tempsDePreparation :' . $A_vue['recette']['tempsDePreparation'] . '</p>';
-echo '<p>difficulte :' . $A_vue['recette']['difficulte'] . '</p>';
-echo '<p>cout :' . $A_vue['recette']['cout'] . '</p>';
-echo '<p>description :' . $A_vue['recette']['description'] . '</p>';
-echo '<p>typeDeCuisson :' . $A_vue['recette']['typeDeCuisson'] . '</p>';
-echo '<p>ingredients :' . var_dump($A_vue['recette']['ingredients']) . '</p>';
-echo '<p>ustensiles :' . var_dump($A_vue['recette']['ustensiles']) . '</p>';
-echo '<p>particularites :' . var_dump($A_vue['recette']['particularites']) . '</p>';
-echo '<p>appreciations :' . var_dump($A_vue['recette']['appreciations']) . '</p>';
 
 Vue::montrer('standard/pied');
