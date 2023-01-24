0d55499ad741d224689114201176463e46c05900
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-24 17:40:58 +0100

    Ajout de la photo de profil

--- Models/Appreciation.php
+++ Models/Appreciation.php
@@ -8,14 +8,14 @@ final class Appreciation extends Model {
 
     public function getAppreciationByIdRecette($I_id_Recette) : array
     {
-        $O_query = $this->getOConnexion()->prepare('SELECT `pseudo`, `id_Recette`, `Appreciation`.`mail`, `note`, `commentaire`, `Appreciation`.`dateDeCreation` FROM `Appreciation`, `Utilisateur` WHERE `Appreciation`.`mail` = `Utilisateur`.`mail` AND `id_Recette` = ?');
+        $O_query = $this->getOConnexion()->prepare('SELECT `pseudo`, `photographie`, `id_Recette`, `Appreciation`.`mail`, `note`, `commentaire`, `Appreciation`.`dateDeCreation` FROM `Appreciation`, `Utilisateur` WHERE `Appreciation`.`mail` = `Utilisateur`.`mail` AND `id_Recette` = ?');
         $O_query->execute(array($I_id_Recette));
 
         return $O_query->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getAppreciationByMail($S_mail) : array {
-        $O_query = $this->getOConnexion()->prepare('SELECT `pseudo`, `id_Recette`, `Appreciation`.`mail`, `note`, `commentaire`, `Appreciation`.`dateDeCreation` FROM `Appreciation`, `Utilisateur` WHERE `Appreciation`.`mail` = `Utilisateur`.`mail` AND `Utilisateur`.`mail` = ?');
+        $O_query = $this->getOConnexion()->prepare('SELECT `pseudo`, `photographie`, `id_Recette`, `Appreciation`.`mail`, `note`, `commentaire`, `Appreciation`.`dateDeCreation` FROM `Appreciation`, `Utilisateur` WHERE `Appreciation`.`mail` = `Utilisateur`.`mail` AND `Utilisateur`.`mail` = ?');
         $O_query->execute(array($S_mail));
 
         return $O_query->fetchAll(PDO::FETCH_ASSOC);
