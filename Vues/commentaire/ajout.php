30cb1de4c3bedde3dab83484b5d1e0e1182e3abc
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-25 23:37:24 +0100

    Début de la page d'ajout de commentaires

---
+++ Vues/commentaire/ajout.php
@@ -0,0 +1,28 @@
+<?php
+echo
+'<img id="logo" class="decorate-img" src="/assets/img/logo.png" alt="Logo of the website : Roi Brioche"/>
+<img id="cookies" class="decorate-img" src="/assets/img/cookies.png" alt="Decoration image representing three cookies"/>
+<img id="donut" class="decorate-img" src="/assets/img/donut.png" alt="Decoration image representing a donut"/>
+
+<main>
+    <h1>Ajout du commentaire</h1>
+
+        <form action=/commentaire/commenter/' . $A_vue['id_Recette'] . ' method="POST">
+            <a id="back-arrow" href="/"><img src="/assets/img/arrow-back-outline.svg" alt="Arrow to return to the home of the website"/></a>
+    
+            <div>
+                <label>Note</label>
+                <input type="range" name="note" min="1" max="5" value="3" step="0.5" oninput="this.nextElementSibling.value = this.value">
+                <output>' . $A_vue['id_Recette'] . '</output>
+            </div>
+            
+            <label>Commentaire</label>
+            <textarea name="commentaire" id="commentaire"></textarea>
+
+            <button id="validate" type="submit">Valider</button>
+        </form>
+        
+    </main>
+</main>
+
+<p id="copyright-text">Copyright © Tous droits réservés Roi Brioche - 2023</p>';
