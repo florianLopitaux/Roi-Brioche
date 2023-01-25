d650cf35c02f63381a25010b1705d8caea91a09c
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-25 23:40:10 +0100

    Ajout de la suppression d'un profil

--- Controleurs/ControleurProfil.php
+++ Controleurs/ControleurProfil.php
@@ -40,6 +40,20 @@ class ControleurProfil
         }
     }
 
+    public function supprimerAction($A_getParam, $A_postParam)
+    {
+        if ($_SESSION['statut'] == 'administrateur' && isset($A_getParam[0]) && $A_getParam[0] != '') {
+            $O_utilisateur = new Utilisateur();
+            $O_utilisateur->deleteUtilisateur($A_getParam[0]);
+            header('Location: /profil');
+            exit();
+        }
+        else {
+            header('Location: /profil');
+            exit();
+        }
+    }
+
     public function showUser(string $mail): void
     {
         $O_utilisateur = new Utilisateur();
