6f7f4f21fdb12d2469f7e8cb610998be8e0d5bad
Jules Combet <jules.combet@etu.univ-amu.fr>
2023-01-25 20:59:00 +0100

    Changement du nom d'un css

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -28,10 +28,10 @@
         echo '<link rel="stylesheet" href="/css/recipe_preview_style.css"/>';
     }
     if ($A_url_param[0] == 'profil') {
-        echo '<link rel="stylesheet" href="/css/voir_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/recipe_style.css"/>';
     }
     if ($A_url_param[1] == 'voir') {
-        echo '<link rel="stylesheet" href="/css/voir_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/recipe_style.css"/>';
     }
     ?>
 
