b62911f3de4f6bccf4ed97715abe92f927a9d79f
Jules <jules.combet@etu.univ-amu.fr>
2023-01-26 16:51:43 +0100

    ajout favicon

--- Vues/gabarit.php
+++ Vues/gabarit.php
@@ -6,37 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Roi Brioche</title>
+    <link rel="icon" href="/assets/img/favicon.ico">
 
     <!-- STYLE FILES -->
-    <link rel="stylesheet" href="/css/global_style.css"/>
+    <link rel="stylesheet" href="/css/global_style.css">
 
     <?php
     $A_url_param = explode('/', $A_vue['url']);
 
     if ($A_url_param[0] == 'index') {
-        echo '<link rel="stylesheet" href="/css/index_style.css"/>';
-        echo '<link rel="stylesheet" href="/css/recipe_preview_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/index_style.css">';
+        echo '<link rel="stylesheet" href="/css/recipe_preview_style.css">';
     }
     if ($A_url_param[0] == 'connexion') {
-        echo '<link rel="stylesheet" href="/css/connexion_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/connexion_style.css">';
     }
     if ($A_url_param[0] == 'inscription') {
-        echo '<link rel="stylesheet" href="/css/inscription_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/inscription_style.css">';
     }
     if ($A_url_param[0] == 'recette') {
-        echo '<link rel="stylesheet" href="/css/recette_style.css"/>';
-        echo '<link rel="stylesheet" href="/css/recipe_preview_style.css"/>';
-        echo '<link rel="stylesheet" href="/css/comment_preview_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/recette_style.css">';
+        echo '<link rel="stylesheet" href="/css/recipe_preview_style.css">';
+        echo '<link rel="stylesheet" href="/css/comment_preview_style.css">';
     }
     if ($A_url_param[0] == 'profil') {
-        echo '<link rel="stylesheet" href="/css/profil_style.css"/>';
-        echo '<link rel="stylesheet" href="/css/comment_preview_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/profil_style.css">';
+        echo '<link rel="stylesheet" href="/css/comment_preview_style.css">';
     }
     if ($A_url_param[1] == 'detail') {
-        echo '<link rel="stylesheet" href="/css/recipe_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/recipe_style.css">';
     }
     if ($A_url_param[1] == 'ajout') {
-        echo '<link rel="stylesheet" href="/css/add_comment_style.css"/>';
+        echo '<link rel="stylesheet" href="/css/add_comment_style.css">';
     }
     ?>
 
