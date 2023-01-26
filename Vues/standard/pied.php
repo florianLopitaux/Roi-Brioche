e4287c773708ca3fc11fdeed042f4d5276f72cfc
Florian <florian.lopitaux@etu.univ-amu.fr>
2023-01-26 17:09:20 +0100

    Add text categories in the footer (whithout css)

--- Vues/standard/pied.php
+++ Vues/standard/pied.php
@@ -16,7 +16,90 @@ echo
         </section>
 
         <section id="right-container">
+            <h3 id="title-section-categories">Liste des Catégories :</h3>
 
+            <section class="list-items-container">
+                <h5 class="title-category">particularités :</h5>
+                <ul class="list-items">
+                    <li class="item">végétarien</li>
+                    <li class="item">végan</li>
+                    <li class="item">sans gluten</li>
+                    <li class="item">sans lactose</li>
+                </ul>
+            </section>
+
+            <section class="list-items-container">
+                <h5 class="title-category">type de cuissons :</h5>
+                <ul class="list-items">
+                    <li class="item">micro-onde</li>
+                    <li class="item">poele</li>
+                    <li class="item">four</li>
+                    <li class="item">fritteuse</li>
+                </ul>
+            </section>
+            
+            <section class="list-items-container">
+                <h5 class="title-category">frouchette de prix :</h5>
+                <ul class="list-items">
+                    <li class="item">bon marché</li>
+                    <li class="item">coût moyen</li>
+                    <li class="item">asser cher</li>
+                </ul>
+            </section>
+
+            <section class="list-items-container">
+                <h5 class="title-category">difficultés :</h5>
+                <ul class="list-items">
+                    <li class="item">très facile</li>
+                    <li class="item">facile</li>
+                    <li class="item">moyen</li>
+                    <li class="item">difficile</li>
+                </ul>
+            </section>
+
+            <section class="list-items-container">
+                <h5 class="title-category">ingrédients :</h5>
+                <ul class="list-items">
+                    <li class="item">farine</li>
+                    <li class="item">sel</li>
+                    <li class="item">lait</li>
+                    <li class="item">sucre</li>
+                    <li class="item">levure chimique</li>
+                    <li class="item">vanille</li>
+                    <li class="item">colorant alimentaire</li>
+                    <li class="item">beurre</li>
+                    <li class="item">canelle</li>
+                    <li class="item">noix de muscade</li>
+                    <li class="item">huile</li>
+                    <li class="item">oeuf</li>
+                    <li class="item">chocolat</li>
+                    <li class="item">caramel</li>
+                    <li class="item">fruits</li>
+                    <li class="item">arome naturel</li>
+                </ul>
+            </section>
+
+            <section class="list-items-container">
+                <h5 class="title-category">ustensiles :</h5>
+                <ul class="list-items">
+                    <li class="item">fouet</li>
+                    <li class="item">couteau</li>
+                    <li class="item">maryse</li>
+                    <li class="item">poche à douille</li>
+                    <li class="item">spatule</li>
+                    <li class="item">robot pâtissier</li>
+                    <li class="item">emporte pièces</li>
+                    <li class="item">moules</li>
+                    <li class="item">verre doseur</li>
+                    <li class="item">balance</li>
+                    <li class="item">cuilière</li>
+                    <li class="item">tamis</li>
+                    <li class="item">cul de poule</li>
+                    <li class="item">tapis de cuisson</li>
+                    <li class="item">râpe</li>
+                    <li class="item">rouleau de pâtisserie</li>
+                </ul>
+            </section>
         </section>
 
         <p id="copyright-text">Copyright © Tous droits réservés Roi Brioche - 2023</p>
