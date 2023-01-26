9f2519347fdbced2f9f801dda7561f9a48304093
Jules <jules.combet@etu.univ-amu.fr>
2023-01-26 15:32:10 +0100

    Ajout d'une page de recherche par utilisateur

--- Controleurs/ControleurProfil.php
+++ Controleurs/ControleurProfil.php
@@ -4,23 +4,7 @@ class ControleurProfil
 {
     public function defautAction()
     {
-        if ($_SESSION['statut'] == 'administrateur' && isset($_GET['search']) && $_GET['search'] != '') {
-            $O_utilisateur = new Utilisateur();
-            $A_utilisateurs = $O_utilisateur->getUserByNameOrEmail($_GET['search']);
-
-            if (sizeof($A_utilisateurs) == 0) {
-                $titre = 'Aucun profil trouvé';
-            } else {
-                $titre = 'Profils (' . sizeof($A_utilisateurs) . ')';
-            }
-
-            Vue::montrer('profil/recherche', array('profils' => $A_utilisateurs, 'titre' => $titre));
-        }
-        elseif ($_SESSION['statut'] == 'utilisateur' && isset($_GET['search']) && $_GET['search'] != '') {
-            header('Location: /profil');
-            exit();
-        }
-        elseif (isset($_SESSION['user'])) {
+        if (isset($_SESSION['user'])) {
             $this->showUser($_SESSION['user']);
         }
         else {
@@ -31,7 +15,7 @@ class ControleurProfil
 
     public function voirAction($A_getParam, $A_postParam)
     {
-        if ($_SESSION['statut'] == 'administrateur' && isset($A_getParam[0]) && $A_getParam[0] != '') {
+        if (isset($A_getParam[0]) && $A_getParam[0] != '') {
             $this->showUser($A_getParam[0]);
         }
         else {
@@ -51,6 +35,24 @@ class ControleurProfil
         else {
             header('Location: /profil');
             exit();
+        }
+    }
+
+    public function rechercheAction($A_getParam, $A_postParam)
+    {
+        if (empty($_GET['search'])) {
+            Vue::montrer('profil/recherche', array('titre' => 'Rechercher un profil par son pseudo ou son email', 'profils' => array()));
+        }
+        elseif (isset($_GET['search']) && $_GET['search'] != '') {
+            $O_utilisateur = new Utilisateur();
+            $A_utilisateurs = $O_utilisateur->getUserByNameOrEmail($_GET['search']);
+
+            if (sizeof($A_utilisateurs) == 0) {
+                $titre = 'Aucun profil trouvé';
+            } else {
+                $titre = 'Profils (' . sizeof($A_utilisateurs) . ')';
+            }
+            Vue::montrer('profil/recherche', array('profils' => $A_utilisateurs, 'titre' => $titre));
         }
     }
 
--- Vues/profil/recherche.php
+++ Vues/profil/recherche.php
@@ -1,11 +1,14 @@
 <?php
 Vue::montrer('standard/header');
 
-echo '<h1 id="title-result">' . $A_vue['titre'] . '</h1>';
+echo '<h1 id="title-result">' . $A_vue['titre'] . '</h1>
+<main>';
 
 foreach ($A_vue['profils'] as $A_profil) {
     Vue::montrer('profil/profilResume', array('profil' =>  $A_profil));
 }
+
+echo '</main>';
 
 Vue::montrer('standard/pied');
 
